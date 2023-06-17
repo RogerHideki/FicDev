ad90f2af64d91d208b271a1b77830857dc7fc4f3
RogerHideki <roger123cont@gmail.com>
2023-06-17 10:55:50 -0400

    0617

--- src/desafio1/src/index.js
+++ src/desafio1/src/index.js
@@ -4,17 +4,17 @@ valido = (caso, valor) => {
     return new Promise((resolve, reject) => {
         if (caso == 1) {
             if (valor === '' || !isNaN(valor)) {
-                reject(new Error('Nome inválido'));
+                reject(new Error('\nNome inválido'));
                 return;
             }
         } else if (caso == 2) {
             if (valor === '') {
-                reject(new Error('Código de registro inválido'));
+                reject(new Error('\nCódigo de registro inválido'));
                 return;
             }
         } else {
             if (valor === '' || !isNaN(valor)) {
-                reject(new Error('Unidade da Federação inválida'));
+                reject(new Error('\nUnidade da Federação inválida'));
                 return;
             }
         }
@@ -22,37 +22,41 @@ valido = (caso, valor) => {
     });
 }
 
-let opcao;
-console.log('\n[1] Incluir nutricionistas');
-console.log('[2] Sair do programa');
-opcao = readline.keyIn('\nEscolha uma das opções. [1, 2]: ', { limit: '12' });
-if (opcao == 1) {
-    let nome = readline.question('Digite o nome da nutricionista: ');
-    valido(1, nome)
-        .then(() => {
-            let crn = readline.question('Digite o código de registro no Conselho Regional da nutricionista: ');
-            valido(2, crn)
-                .then(() => {
-                    let uf = readline.question('Digite a Unidade da Federação da nutricionista: ');
-                    valido(3, uf)
-                        .then(() => {
-                            (async () => {
-                                const Sequelize = require('sequelize');
-                                const Op = Sequelize.Op;
-                                const database = require('./database/config/db');
-                                const DadosDeNutricionistas = require('./database/models/dadosdenutricionistas');
-                                await database.sync();
+async function main() {
+    let opcao;
+    do {
+        console.log('\n[1] Incluir nutricionistas');
+        console.log('[2] Sair do programa');
+        opcao = readline.keyIn('\nEscolha uma das opções. [1, 2]: ', { limit: '12' });
+        if (opcao == 1) {
+            let nome = readline.question('\nDigite o nome da nutricionista: ');
+            await valido(1, nome)
+                .then(async () => {
+                    let crn = readline.question('\nDigite o código de registro no Conselho Regional da nutricionista: ');
+                    await valido(2, crn)
+                        .then(async () => {
+                            let uf = readline.question('\nDigite a Unidade da Federação da nutricionista: ');
+                            await valido(3, uf)
+                                .then(async () => {
+                                    const Sequelize = require('sequelize');
+                                    const Op = Sequelize.Op;
+                                    const database = require('./database/config/db');
+                                    const DadosDeNutricionistas = require('./database/models/dadosdenutricionistas');
+                                    await database.sync();
 
-                                const dadosDeNutricionistas = await DadosDeNutricionistas.create({
-                                    nome: `${nome}`,
-                                    crn: `${crn}`,
-                                    uf: `${uf}`
+                                    const dadosDeNutricionistas = await DadosDeNutricionistas.create({
+                                        nome: `${nome}`,
+                                        crn: `${crn}`,
+                                        uf: `${uf}`
+                                    })
                                 })
-                            })();
+                                .catch(e => console.error(e.message))
                         })
                         .catch(e => console.error(e.message))
                 })
                 .catch(e => console.error(e.message))
-        })
-        .catch(e => console.error(e.message))
+        }
+    } while (opcao != 2);
 }
+
+main();
