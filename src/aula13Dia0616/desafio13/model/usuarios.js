52150de8326d8dea9d36cbae1babb76fc45e04d5
RogerHideki <roger123cont@gmail.com>
2023-06-21 09:07:11 -0400

    0621

---
+++ src/aula13Dia0616/desafio13/model/usuarios.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize')
+const database = require('../db/db')
+
+const Usuario = database.define('usuarios', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+})
+
+module.exports = Usuario
